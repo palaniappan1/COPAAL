238930f2f38ce93c2aa003f6e6cabe47e707899a
palaniappan1 <palaniappan.pp@gmail.com>
2022-05-16 18:22:21 +0200

    Written dockerfile for COPAAL and try to invoke run from docker

---
+++ service/EntryPoint.java
@@ -0,0 +1,9 @@
+public class EntryPoint {
+    public static void print(){
+        System.out.println("EntryPoint docker");
+    }
+
+    public static void main(String[] args){
+        print();
+    }
+}
